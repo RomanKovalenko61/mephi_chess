0d3483d9777e4ac50fba0f8c4729e3e39cb52f54
Roman <astek14@mail.ru>
2024-11-10 16:55:16 +0300

    Logic Pawn

--- src/ru/kovalenko/chess/Pawn.java
+++ src/ru/kovalenko/chess/Pawn.java
@@ -7,6 +7,12 @@ public class Pawn extends ChessPiece {
 
     @Override
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (chessBoard.board[line][column].getColor().equals("White")) {
+            return line == 1 && toLine == 3 && column == toColumn || line + 1 == toLine && column == toColumn;
+        }
+        if (chessBoard.board[line][column].getColor().equals("Black")) {
+            return line == 6 && toLine == 4 && column == toColumn || line - 1 == toLine && column == toColumn;
+        }
         return false;
     }
 
