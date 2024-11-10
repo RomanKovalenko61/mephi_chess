1951a5772f4f12c0fb8d1efb2c9857acde4e63ab
Roman <astek14@mail.ru>
2024-11-10 19:08:02 +0300

    Logic Rook

--- src/ru/kovalenko/chess/Rook.java
+++ src/ru/kovalenko/chess/Rook.java
@@ -7,7 +7,9 @@ public class Rook extends ChessPiece {
 
     @Override
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return false;
+        int diffLine = Math.abs(line - toLine);
+        int diffColumn = Math.abs(column - toColumn);
+        return diffLine > 0 && column == toColumn || diffColumn > 0 && line == toLine;
     }
 
     @Override
