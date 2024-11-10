2897b502f75ac186a25100c80287b9e3b96b5804
Roman <astek14@mail.ru>
2024-11-10 20:11:49 +0300

    update Logic Queen

--- src/ru/kovalenko/chess/Queen.java
+++ src/ru/kovalenko/chess/Queen.java
@@ -9,13 +9,10 @@ public class Queen extends ChessPiece {
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         int diffLine = Math.abs(line - toLine);
         int diffColumn = Math.abs(column - toColumn);
-        if (diffLine == 0 && diffColumn == 1 || diffLine == 1 && diffColumn == 0 || diffLine == 1 && diffColumn == 1) {
-            return true;
-        }
         if (diffLine == diffColumn && diffLine != 0) {
             return true;
         }
-        return diffLine == 0 && diffColumn > 1 || diffLine > 1 && diffColumn == 0;
+        return diffLine == 0 && diffColumn > 0 || diffLine > 0 && diffColumn == 0;
     }
 
     @Override
