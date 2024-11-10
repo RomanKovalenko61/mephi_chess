a60006c9e6b3a2a6b7a87b137fe1c0587ed30832
Roman <astek14@mail.ru>
2024-11-10 16:18:14 +0300

    Horse logic

--- src/ru/kovalenko/chess/Horse.java
+++ src/ru/kovalenko/chess/Horse.java
@@ -7,6 +7,15 @@ public class Horse extends ChessPiece {
 
     @Override
     boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (line == toLine && column == toColumn) {
+            return false;
+        }
+        if (line - 1 == toLine || line + 1 == toLine) {
+            return column - 2 == toColumn || column + 2 == toColumn;
+        }
+        if (line - 2 == toLine || line + 2 == toLine) {
+            return column - 1 == toColumn || column + 1 == toColumn;
+        }
         return false;
     }
 
